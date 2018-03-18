ae7e86d31dff56f9ed054aeb8338278380aaaeb7
farhaadw <farhaad.wasim@googlemail.com>
2018-03-18 19:48:52 +0000

    Added init files

---
+++ src/main/java/com/qa/bookstore/Book.java
@@ -0,0 +1,143 @@
+package com.qa.bookstore;
+
+import javax.persistence.*;
+import java.util.Date;
+import java.util.Objects;
+
+@Entity
+public class Book {
+
+    @Id @GeneratedValue
+    private Long id;
+
+    @Column(length = 200)
+    private String title;
+
+    @Column(length = 1000)
+    private String description;
+
+    @Column(name = "unit_cost")
+    private Float unitCost;
+
+    private String isbn;
+
+    @Column(name = "publication__date")
+    @Temporal(TemporalType.DATE)
+    private Date publicationDate;
+
+
+    private Integer noOfPages;
+
+    @Column(name = "image_url")
+    private String imageUrl;
+
+    private Language language;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Float getUnitCost() {
+        return unitCost;
+    }
+
+    public void setUnitCost(Float unitCost) {
+        this.unitCost = unitCost;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public Date getPublicationDate() {
+        return publicationDate;
+    }
+
+    public void setPublicationDate(Date publicationDate) {
+        this.publicationDate = publicationDate;
+    }
+
+    public Integer getNoOfPages() {
+        return noOfPages;
+    }
+
+    public void setNoOfPages(Integer noOfPages) {
+        this.noOfPages = noOfPages;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+
+    public Language getLanguage() {
+        return language;
+    }
+
+    public void setLanguage(Language language) {
+        this.language = language;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Book book = (Book) o;
+        return Objects.equals(id, book.id) &&
+                Objects.equals(title, book.title) &&
+                Objects.equals(description, book.description) &&
+                Objects.equals(unitCost, book.unitCost) &&
+                Objects.equals(isbn, book.isbn) &&
+                Objects.equals(publicationDate, book.publicationDate) &&
+                Objects.equals(noOfPages, book.noOfPages) &&
+                Objects.equals(imageUrl, book.imageUrl) &&
+                language == book.language;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, title, description, unitCost, isbn, publicationDate, noOfPages, imageUrl, language);
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", description='" + description + '\'' +
+                ", unitCost=" + unitCost +
+                ", isbn='" + isbn + '\'' +
+                ", publicationDate=" + publicationDate +
+                ", noOfPages=" + noOfPages +
+                ", imageUrl='" + imageUrl + '\'' +
+                ", language=" + language +
+                '}';
+    }
+}
---
+++ src/main/java/com/qa/bookstore/Language.java
@@ -0,0 +1,5 @@
+package com.qa.bookstore;
+
+public enum Language {
+    ENGLISH, FRENCH, SPANISH, PORTUGESE, ITALIAN, FINISH, GERMAN, DEUTSCH, RUSSIAN
+}
